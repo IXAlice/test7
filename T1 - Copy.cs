0270a585cc0a103e220c56d69d4a53ec8972f860
IX <warodom.nine@actieon.com>
2025-01-30 18:24:59 +0700

    commit3

--- T1 - Copy.cs
+++ T1 - Copy.cs
@@ -7,10 +7,7 @@ namespace HelloWorld
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
-      Console.WriteLine("Branch1");        
-      
-      Console.WriteLine("Hello World!");    
-      Console.WriteLine("Main");   
+
     }
   }
 }
